f6cfc5fc299f061f4d945118a74be2753b6a13d6
339513533 <339513533@2260I-L29-022>
2023-11-01 10:31:12 -0400

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_A_Die_Class
+{
+    public class Die
+    {
+        private int _roll;
+        private Random _generator;
+
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random();
+            _roll = roll;
+        }
+
+
+
+        public override string ToString()
+        {
+            return _roll + "";
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawDie()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            } 
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("-----");
+
+            }
+            else if (Roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("| o |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("-----");
+                
+            }
+            else if (Roll == 4)
+            {
+               Console.WriteLine("-----");
+               Console.WriteLine("|o o|");
+               Console.WriteLine("|   |");
+               Console.WriteLine("|o o|");
+               Console.WriteLine("-----");
+
+            }
+            else if (Roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+
+            }
+            else if (Roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| ¿ |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,266 @@
+﻿using Making_A_Die_Class;
+using System.Net.Security;
+
+namespace The_Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double bankAccount = 100.00, bet;
+            string userInputMenu = "";
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine($"Hello, you have ${bankAccount} dollars what do you want to bet?");
+                Console.WriteLine("You can bet...");
+                Console.WriteLine("1 .. Doubles (win double what you bet)");
+                Console.WriteLine("2 .. Not double (win half your bet)");
+                Console.WriteLine("3 .. Even sum (win your bet, you bet 3 you get 3)");
+                Console.WriteLine("4 .. Odd sum (Same as even sum just odd)");
+                Console.WriteLine("Please type numbers not words.");
+                userInputMenu = Console.ReadLine();
+                if (userInputMenu == "1")
+                {
+                    Console.WriteLine("You picked 1, please place a bet");
+                    repeat = false;
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    if (bet > bankAccount)
+                    {
+                        bet = bankAccount;
+                    }
+                    if (bet < 0)
+                    {
+                        bet = 0;
+                    }
+
+                    Console.ReadLine();
+                    Die die1 = new Die();
+                    Die die2 = new Die(3);
+
+                    Console.WriteLine($"You rolled a {die1}");
+                    die1.DrawDie();
+
+
+                    die1.RollDie();
+                    die1.DrawDie();
+                    Console.WriteLine($"You rolled a {die1}");
+                    if (die1.Roll == die2.Roll)
+                    {
+                        bankAccount = bankAccount + bet * 2;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                    }
+                    else if (die1.Roll != die2.Roll)
+                    {
+                        if (bankAccount < bet * 2)
+                        {
+                            bankAccount = 0;
+                        }
+                        else if (bankAccount > bet * 2)
+                        {
+                            bankAccount = bankAccount - bet * 2;
+                        }
+
+
+                        Console.WriteLine("YOU LOST...");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                }
+                else if (userInputMenu == "2")
+                {
+                    Console.WriteLine("You picked 2, please place a bet");
+                    repeat = false;
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    if (bet > bankAccount)
+                    {
+                        bet = bankAccount;
+                    }
+                    if (bet < 0)
+                    {
+                        bet = 0;
+                    }
+
+                    Console.ReadLine();
+                    Die die1 = new Die();
+                    Die die2 = new Die(3);
+                    Console.WriteLine($"You rolled a {die1}");
+                    die1.DrawDie();
+
+
+                    die1.RollDie();
+                    die1.DrawDie();
+                    Console.WriteLine($"You rolled a {die1}");
+
+                    if (die1.Roll == die2.Roll)
+                    {
+                        bankAccount = bankAccount - bet / 2;
+
+
+                        Console.WriteLine("YOU LOST...");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1 != die2)
+                    {
+                        bankAccount = bankAccount + bet / 2;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                }
+                else if (userInputMenu == "3")
+                {
+                    Console.WriteLine("You picked 3, please place a bet");
+                    repeat = false;
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    if (bet > bankAccount)
+                    {
+                        bet = bankAccount;
+                    }
+                    if (bet < 0)
+                    {
+                        bet = 0;
+                    }
+                    Console.ReadLine();
+                    Die die1 = new Die();
+                    Die die2 = new Die(3);
+                    Console.WriteLine($"You rolled a {die1}");
+                    die1.DrawDie();
+
+
+                    die1.RollDie();
+                    die1.DrawDie();
+                    Console.WriteLine($"You rolled a {die1}");
+                    if (die1.Roll + die2.Roll == 2)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 4)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 6)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 8)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else  if (die1.Roll + die2.Roll == 10)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else  if (die1.Roll + die2.Roll == 12)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOU LOST...");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+
+                }
+                else if (userInputMenu == "4")
+                {
+                    Console.WriteLine("You picked 4, please place a bet");
+                    repeat = false;
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    if (bet > bankAccount)
+                    {
+                        bet = bankAccount;
+                    }
+                    if (bet < 0)
+                    {
+                        bet = 0;
+                    }
+                    Console.ReadLine();
+                    Die die1 = new Die();
+                    Die die2 = new Die(3);
+                    Console.WriteLine($"You rolled a {die1}");
+                    die1.DrawDie();
+
+
+                    die1.RollDie();
+                    die1.DrawDie();
+                    Console.WriteLine($"You rolled a {die1}");
+                    if (die1.Roll + die2.Roll == 1)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 3)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 5)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 7)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 9)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else if (die1.Roll + die2.Roll == 11)
+                    {
+                        bankAccount = bankAccount + bet;
+                        Console.WriteLine("YOU WON!!");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOU LOST...");
+                        Console.WriteLine($"You now have ${bankAccount}!");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
